b4a0b98054f6d712dfe42a42bfd30fd7fcabad99
Landon Carter <lcarter@palantir.com>
2018-09-19 21:45:24 -0400

    i think this fixes instagram and tidies up the python directory

--- Python/instaGrabber.py
+++ Python/instaGrabber.py
@@ -1,11 +1,9 @@
 import requests, json
 
-try: 
+try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
-
-from InstagramAPI import InstagramAPI
 
 import config
 
@@ -24,31 +22,28 @@ import config
 def instafeed(hashtag, count):
     """Simply use with autoescapeoff, for example: {% autoescape off %}{% instafeed 'manifeste16' 8 %}{% endautoescape %}"""
     feed = []
-    
+
     html = ''
     main_url = 'https://www.instagram.com'
     query_url = main_url + '/explore/tags/%s' % (hashtag,)
-    # params = 'ig_hashtag(%s){media.first(%s){nodes{caption,display_src}}}' % (hashtag, str(count))
-    # item_html = '<div class="box-item-25"><a href="%s" target="_blank"><img src="%s" alt="%s" title="%s"></a></div>'
-    # print(query_url + params)
-    # r = requests.get(query_url + params)
     r = requests.get(query_url)
     parsed = BeautifulSoup(r.content)
-    # j = json.loads(r.content.decode('utf-8'))
-    
+
     scriptstuff = parsed.findAll('script')
     scriptstuff.sort(key=lambda x: len(x.text))
     actualScript = scriptstuff[-1].text[21:-1]
     j = json.loads(actualScript)
-    media = j['entry_data']['TagPage'][0]['tag']['media']['nodes']
-    for image in media:
-        caption = image['caption']
-        display_src = image['display_src']
+    # print(j)
+    # print(j['entry_data']['TagPage'][0].keys())
+    elements = j['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+    nodes = [element['node'] for element in elements]
+    for node in nodes:
+        caption = node['edge_media_to_caption']['edges'][0]['node']['text']
+        display_src = node['display_url']
         feed.append({'caption': caption, 'display_src': display_src})
 
 
-    file = open("/Volumes/courses/2.009/2.009, 2017/infoPappa/display/public/insta.json", "w")
-    json.dump(feed, file)
-    file.close()
+    with open("/Volumes/courses/2.009/2.009, 2018/infoPappa/display/public/insta.json", "w") as f:
+        json.dump(feed, f)
 
 instafeed('009mit', 16);
